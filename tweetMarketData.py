d9f80a04f4d96ca1e0bc70f1723ab5124b37b17f
bluedaze <35676690+bluedaze@users.noreply.github.com>
2019-11-22 15:09:33 -0500

    Add files via upload

---
+++ tweetMarketData.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from pprint import pprint
+
+page = requests.get('https://www.predictit.org/api/marketdata/all/')
+all_markets = json.loads(page.text)
+markets = all_markets['markets']
+#pprint(markets)
+for i in markets:
+    marketName = i['name']
+    marketID = i['id']
+    marketURL = i['url']
+    contracts = i['contracts']
+    if (marketName.find('tweets') != -1):
+        #pprint(contracts)
+        print("Market ID:", marketID)
+        print(marketName)
+        print(marketURL)
+        for i in contracts:
+            betName = i['name']
+            bestBuyNo = i['bestBuyNoCost']
+            bestBuyYes = i['bestBuyYesCost']
+            bestSellNo = i['bestSellNoCost']
+            bestSellYes = i['bestSellYesCost']
+            print()
+            print("-------")
+            print(betName)
+            print("-------")
+            print("Buy no:", bestBuyNo)
+            print("Buy yes:", bestBuyYes)
+            print("Sell no:", bestSellNo)
+            print("Sell yes:", bestSellYes)
+        print()
