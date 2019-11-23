9d950efa7b628d1c863a12311300fb5dfc887b45
bluedaze <35676690+bluedaze@users.noreply.github.com>
2019-11-22 23:52:30 -0500

    Add files via upload

--- tweetMarketData.py
+++ tweetMarketData.py
@@ -1,59 +1,65 @@
 #!/usr/bin/python3
+import time
 import requests
 import json
 from pprint import pprint
 
-page = requests.get('https://www.predictit.org/api/marketdata/all/')
-all_markets = json.loads(page.text)
-markets = all_markets['markets']
-#pprint(markets)
-for i in markets:
-    marketName = i['name']
-    timeStamp = i['timeStamp']
-    marketID = i['id']
-    marketURL = i['url']
-    marketURL = "See further details here: " + marketURL
-    contracts = i['contracts']
-    if (marketName.find('tweets') != -1):
-        #pprint(contracts)
-        print("\n\n")
-        divider = "[~~~~~~~~~~~~~~~~~~~~~~~~~~~]"
-        print(divider.center(120))
-        header = "Market ID: " + str(marketID)
-        print(header.center(120))
-        print(timeStamp.center(120))
-        print()
-        print(marketName.center(120))
-        for i in contracts:
-            betName = i['name']
-            bestBuyNo = i['bestBuyNoCost']
-            bestBuyYes = i['bestBuyYesCost']
-            bestSellNo = i['bestSellNoCost']
-            bestSellYes = i['bestSellYesCost']
+count = 0
+while True:
+    count = count + 1
+    page = requests.get('https://www.predictit.org/api/marketdata/all/')
+    all_markets = json.loads(page.text)
+    markets = all_markets['markets']
+    #pprint(markets)
+    for i in markets:
+        marketName = i['name']
+        timeStamp = i['timeStamp']
+        marketID = i['id']
+        marketURL = i['url']
+        marketURL = "See further details here: " + marketURL
+        contracts = i['contracts']
+        if (marketName.find('tweets') != -1):
+            #pprint(contracts)
+            print("\n\n")
+            divider = "[~~~~~~~~~~~~~~~~~~~~~~~~~~~]"
+            print(divider.center(120))
+            header = "Market ID: " + str(marketID)
+            print(header.center(120))
+            print(timeStamp.center(120))
             print()
-            print("-------")
-            print(betName)
-            print("-------")
+            print(marketName.center(120))
+            for i in contracts:
+                betName = i['name']
+                bestBuyNo = i['bestBuyNoCost']
+                bestBuyYes = i['bestBuyYesCost']
+                bestSellNo = i['bestSellNoCost']
+                bestSellYes = i['bestSellYesCost']
+                print()
+                print("-------")
+                print(betName)
+                print("-------")
 
-            if bestBuyNo == None:
-                print("Buy no: N/A")
-            else:
-                print("Buy no:", bestBuyNo)
+                if bestBuyNo == None:
+                    print("Buy no: N/A")
+                else:
+                    print("Buy no:", bestBuyNo)
 
-            if bestBuyYes == None:
-                print("Buy yes: N/A")
-            else:
-                print("Buy yes:", bestBuyYes)
+                if bestBuyYes == None:
+                    print("Buy yes: N/A")
+                else:
+                    print("Buy yes:", bestBuyYes)
 
-            if bestSellNo == None:
-                print("Sell no: N/A")
-            else:
-                print("Sell no:", bestSellNo)
+                if bestSellNo == None:
+                    print("Sell no: N/A")
+                else:
+                    print("Sell no:", bestSellNo)
 
-            if bestSellYes == None:
-                print("Sell yes: N/A")
-            else:
-                print("Sell yes:", bestSellYes)
-        print("\n")
-        print(marketURL.center(100))
-        print()
+                if bestSellYes == None:
+                    print("Sell yes: N/A")
+                else:
+                    print("Sell yes:", bestSellYes)
+            print("\n")
+            print(marketURL.center(100))
+            print()
+    print(count)
+    time.sleep(60)
