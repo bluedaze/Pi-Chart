825a9d29339f782a46eb0e3a139450d95c8c4b2b
bluedaze <35676690+bluedaze@users.noreply.github.com>
2020-05-28 16:44:57 -0400

    Add files via upload

---
+++ update-pi.sh
@@ -0,0 +1,10 @@
+echo "Updating pi-graph.com..."
+sleep 1s
+echo "Pulling from repository"
+git pull https://github.com/bluedaze/Pi-Chart.git
+sleep 4s
+echo "Killing green unicorns. Die unicorn, die!"
+pkill -f gunicorn
+sleep 2s
+echo "Spawning three new gunicorn babies"
+gunicorn -w 3 pigraph:app
