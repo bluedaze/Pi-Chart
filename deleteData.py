278602f2fb3c28e22fffbaa9e7f7da3bd18f7ec0
bluedaze <35676690+bluedaze@users.noreply.github.com>
2020-05-29 19:36:06 -0400

    Add files via upload

--- deleteData.py
+++ deleteData.py
@@ -2,13 +2,17 @@ import sqlite3
 from datetime import datetime, timedelta
 from datetime import datetime, timedelta
 
+
 today = datetime.today() 
 yesterday = today - timedelta(hours=24)
-print(yesterday)
 today.strftime('%Y-%m-%d')
-yesterday.strftime('%Y-%m-%d')
+yesterday = yesterday.strftime('%Y-%m-%d')
+print("Purging data from", yesterday)
 conn = sqlite3.connect("pidb.db", isolation_level=None)
 conn.execute('pragma journal_mode=wal;')
 c = conn.cursor()
 c.execute("DELETE FROM tweets WHERE timeStamp LIKE (?)", (yesterday,))
 conn.close()
+print()
+print()
+print("Purging completed.")
