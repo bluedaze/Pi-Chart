e5bf856b44c10d9668908b3f66f411085ba5e3b0
bluedaze <35676690+bluedaze@users.noreply.github.com>
2020-06-01 11:52:06 -0400

    Add files via upload
    Refactored incomprehensible code.

--- pisql.py
+++ pisql.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 
+
 def query_bracket(market):
 	''' Query database for tweet markets '''
 	conn = sqlite3.connect("pidb.db", isolation_level=None)
@@ -11,8 +12,8 @@ def query_bracket(market):
 	bracketData = {}
 	for i in range(9):
 		bracket = "B"+str(i+1)
-		b = [(y, z) for x, y, z in selected if x == bracket]
-		prices, timeStamp = map(list, zip(*[[k, v] for k, v in b]))
+		prices = [(item[1]) for item in selected if item[0] == bracket]
+		timeStamp = [(item[2]) for item in selected if item[0] == bracket]
 		bracketData[bracket] = prices, timeStamp
 	c.close()
 	conn.close()
@@ -22,22 +23,14 @@ def fetch_tables():
 	tables = []
 	''' Gets the names of all markets '''
 	conn = sqlite3.connect("pidb.db", isolation_level=None)
-	conn.execute('pragma journal_mode=wal;')
 	c = conn.cursor()
 	c.execute("SELECT DISTINCT marketName FROM tweets")
 	selected = c.fetchall()
 
 	for item in selected:
-	    tables.append(item[0])
+	  tables.append(item[0])
 
 	c.close()
 	conn.close()
 
 	return tables
-
-# stuff = query_bracket("B1", "realDonaldTrump")
-
-# for i in stuff:
-# 	print(i)
-
-# print(stuff)
