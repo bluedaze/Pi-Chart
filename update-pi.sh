0fab003a9c1008088a88ff59fcffca890ea4e4fa
bluedaze <35676690+bluedaze@users.noreply.github.com>
2020-06-05 11:27:08 -0400

    Update update-pi.sh

--- update-pi.sh
+++ update-pi.sh
@@ -1,4 +1,4 @@
-echo "Updating pi-graph.com..."
+cho "Updating pi-graph.com..."
 sleep 1s
 echo "Pulling from repository"
 git pull https://github.com/bluedaze/Pi-Chart.git
@@ -7,4 +7,9 @@ echo "Killing green unicorns. Die unicorn, die!"
 pkill -f gunicorn
 sleep 2s
 echo "Spawning three new gunicorn babies"
-gunicorn -w 3 pigraph:app
+gunicorn -w 3 pigraph:app &
+sleep 2s
+clear
+sleep 1s
+ps -ef | grep gunicorn
+ps -ef | grep python
